bcfbe44d013f9f696154a1ae44a9b87c589397f8
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-11-25 00:08:50 -0500

    clean slate

--- src/app/api/data/route.js
+++
@@ -1,3 +0,0 @@
-export async function GET(request) {
-  return new Response('Empyrean Ecommerce - Fullstack MERN website with Tailwind, AWS, and Stripe');
-}
--- src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import Home from "./home/page";
-
-export default function App() {
-  return (
-    <div className="">
-        <Home />
-    </div>
-  )
-}
